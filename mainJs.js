faee433a3485d95814b504bf521694ed337dbd2d
jpannu10 <jpannu10@gmail.com>
2019-10-06 11:16:37 -0800

    Update mainJs.js

--- mainJs.js
+++ mainJs.js
@@ -5,25 +5,29 @@ let sec = document.querySelector('.sec');
 let PaM = document.querySelector('.PM');
 
 
-
-
-//Getting dates from Date Object
-let dateObject = new Date();
-
- hour.innerText = dateObject.getHours();
- min.innerHTML  = dateObject.getMinutes();
+var secStartObject = new Date().getSeconds();
+sec.innerText =  secStartObject;
 
   //settingSeconds
 setInterval(()=>{
-    
-   
-    
     let secObject = new Date().getSeconds();
     sec.innerText =  secObject;
-    
-   
-   
 },1000);
+
+//setting-minutes
+setInterval(()=>{
+    let minObject = new Date().getMinutes();
+    min.innerHTML  = minObject;
+},1000);
+
+//setting-hours
+setInterval(()=>{
+    let houObject = new Date().getHours();
+    hour.innerText = houObject;
+
+},1000)
+
+
 
 if(hour.innerText > 12){
     PaM.innerHTML ='PM';
